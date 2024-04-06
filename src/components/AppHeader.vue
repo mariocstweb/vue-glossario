d0ac2c0ea5c92f848cff65f4dd2d9cb7111080ec
mariocstweb <mariocstmra@gmail.com>
2024-04-06 16:26:17 +0200

    created AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
+
+
+
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <RouterLink class="nav-link active" aria-current="page" :to="'/'">Home</RouterLink>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+
+
+<style scoped></style>
