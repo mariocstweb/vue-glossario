22bb6e2f63176e50bd8034d0c2a4cb555d8aa893
mariocstweb <mariocstmra@gmail.com>
2024-04-06 16:14:41 +0200

    testing axios

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,34 @@
-<script setup>
+<script>
+import axios from 'axios';
+const endpoint = 'http://localhost:8000/api/words';
 
+export default {
+  name: 'Glossario',
+  components: {},
+  data: () => ({ words: [] }),
+  methods: {
+    fetchWords() {
+      axios.get(endpoint)
+        .then(res => {
+          this.words = res.data.data;
+        })
+        .catch(err => {
+          console.error(err.message);
+        });
+    }
+  },
+  created() {
+    this.fetchWords();
+  }
+}
 </script>
 
 <template>
-  <h1 class="text-center">Ciao</h1>
+  <main class="container">
+    <ul>
+      <li v-for="word in words">{{ word.title }}</li>
+    </ul>
+  </main>
 </template>
 
 <style lang="scss" scoped></style>
