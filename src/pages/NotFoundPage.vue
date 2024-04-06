0bbc01c3f56ca136524074c965c35655e79abe8e
mariocstweb <mariocstmra@gmail.com>
2024-04-06 16:27:39 +0200

    created notfoundepage

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: 'NotFoundPage'
+}
+</script>
+
+<template>
+  <div class="vh-100 d-flex justify-content-center align-items-center flex-column">
+    <h1 class="text-center">404 | Page Not Found</h1>
+  </div>
+</template>
+
+<style></style>
