4b53b673f9ebae5299daeca2cb930d81c92ea2fc
mariocstweb <mariocstmra@gmail.com>
2024-04-06 16:26:28 +0200

    Added loader

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <script>
 import axios from 'axios';
+import AppLoader from './components/AppLoader.vue';
+import AppHeader from './components/AppHeader.vue';
 const endpoint = 'http://localhost:8000/api/words';
 
 export default {
   name: 'Glossario',
-  components: {},
-  data: () => ({ words: [] }),
+  components: { AppHeader, AppLoader },
+  data: () => ({ words: [], isLoading: false }),
   methods: {
     fetchWords() {
+      this.isLoading = true;
       axios.get(endpoint)
         .then(res => {
           this.words = res.data.data;
         })
         .catch(err => {
           console.error(err.message);
+        }).then(() => {
+          this.isLoading = false;
         });
     }
   },
@@ -24,6 +29,8 @@ export default {
 </script>
 
 <template>
+  <AppHeader />
+  <AppLoader v-if="isLoading" />
   <main class="container">
     <ul>
       <li v-for="word in words">{{ word.title }}</li>
---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import {createRouter, createWebHistory} from 'vue-router';
+//  import NotFoundPage from '../pages/NotFoundPage.vue';
+
+
+//  Definisco le rotte
+ export default createRouter({
+  history: createWebHistory(),
+  routes:[
+    // {path: '/', component: HomePage},
+    // {path: '/projects/:id', component: ShowPage, name: 'ShowPage'},
+    // {path: '/:pathMatch(.*)*', component: NotFoundPage},
+  ]
+ });
